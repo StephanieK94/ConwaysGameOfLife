40931072fb008e5c59397952ea239d8e7f5ccb84
StephanieK <stephanie.kitchen@serko.com>
2019-08-06 14:21:38 +1200

    Second test to ensure. Refactoring to reduce duplication

--- ConwaysGameOfLife.Tests/GridTests.cs
+++ ConwaysGameOfLife.Tests/GridTests.cs
@@ -5,16 +5,33 @@ namespace ConwaysGameOfLife.Tests
 {
     public class GridTests
     {
+        private readonly Game game;
+        public GridTests()
+        {
+            game = new Game();
+        }
+
         [Fact]
         public void GivenNewGame_WhenCallsGridOf1By1_ReturnsArrayOfSingleBoolean ()
         {
-            Game game = new Game();
-
             var actual = game.Grid(1,1);
-
             var expected = new bool[1 , 1] { { false } };
 
             Assert.Equal( expected, actual );
+        }
+
+        [Fact]
+        public void GivenNewGame_WhenCallsGridOf3By3_ReturnsArrayOf3By3Boolean ()
+        {
+            var actual = game.Grid( 3,3 );
+            var expected = new bool[3,3] 
+            { 
+                { false,false,false },
+                { false , false , false }, 
+                { false , false , false }
+            };
+
+            Assert.Equal( expected , actual );
         }
     }
 }
